b5022bb43ef17754de4844373b802b35fb40ccde
Engineer-of-Stuff <dseal15@gmail.com>
2022-09-16 16:30:41 -0600

    Guide archives
    Former-commit-id: 7a325629004f95094234a89f86cffdf4f0d0dec2

---
+++ Docs/Archives/update-guides.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wget "https://rentry.org/voldy/pdf" -O "VOLDY RETARD GUIDE.pdf"
+wget "https://rentry.org/drfar/pdf" -O "Inpainging and Outpainting.pdf"
+wget "https://rentry.org/textard/pdf" -O "RETARD'S GUIDE TO TEXTUAL INVERSION.pdf"
+wget "https://rentry.org/anime_and_titties/pdf" -O "big titty anon's list of artists.pdf"
+wget "https://rentry.org/informal-training-guide/pdf" -O "Informal Training Guide.pdf"
+wget "https://rentry.org/865dy/pdf" -O "Getting Started on Paperspace.pdf"
+wget "https://rentry.org/cputard/pdf" -O "CPU RETARD GUIDE.pdf"
+wget "https://rentry.org/sd-nativeisekai/pdf" -O "Stable Diffusion Native Isekai.pdf"
+wget "https://rentry.org/ayymd-stable-diffustion-v1_4-guide/pdf" -O "AyyMD Stable Diffuse v1.4 for Wangblows 10.pdf"
+wget "https://rentry.org/sdamd/pdf" -O "Stable Diffusion AMD.pdf"
+wget "https://rentry.org/yrpvv/pdf" -O "Standard Diffusion Models.pdf"
+wget "https://rentry.org/sdupscale" -O "Larger resolutions with Stable Diffusion.pdf"
