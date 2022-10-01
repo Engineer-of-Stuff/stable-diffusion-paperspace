b4cd485bc155f102fe31c8fc90d38baf9db907f5
frostydad <64224601+Cyberes@users.noreply.github.com>
2022-10-01 11:51:28 -0600

    update archives
    Former-commit-id: 3151c735168544398fa0ec7e4f7ba78873208ae2 [formerly 4e266869f22b9666bd5b3f16baa59886a5bfdbed]
    Former-commit-id: 21c0c937eb8cc1ae7cf99c0cce63e501f9b5f51a

--- docs/archives/update-guides.sh
+++ docs/archives/update-guides.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Don't install with apt. It segfaults
+# wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
+# sudo apt install xfonts-75dpi
+# sudo dpkg -i wkhtmltox_0.12.6-1.focal_amd64.deb
+
 wget "https://rentry.org/voldy/pdf" -O "VOLDY RETARD GUIDE.pdf"
 wget "https://rentry.org/drfar/pdf" -O "Inpainging and Outpainting.pdf"
 wget "https://rentry.org/textard/pdf" -O "RETARD'S GUIDE TO TEXTUAL INVERSION.pdf"
@@ -11,5 +16,7 @@ wget "https://rentry.org/sd-nativeisekai/pdf" -O "Stable Diffusion Native Isekai
 wget "https://rentry.org/ayymd-stable-diffustion-v1_4-guide/pdf" -O "AyyMD Stable Diffuse v1.4 for Wangblows 10.pdf"
 wget "https://rentry.org/sdamd/pdf" -O "Stable Diffusion AMD.pdf"
 wget "https://rentry.org/yrpvv/pdf" -O "Stable Diffusion Models.pdf"
-wget "https://rentry.org/sdupscale" -O "Larger resolutions with Stable Diffusion.pdf"
-wget "https://rentry.org/male-to-anime-girl" -O "How to Turn Yourself Into a Cute Anime Girl.pdf"
+wget "https://rentry.org/sdupscale/pdf" -O "Larger resolutions with Stable Diffusion.pdf"
+wget "https://rentry.org/male-to-anime-girl/pdf" -O "How to Turn Yourself Into a Cute Anime Girl.pdf"
+wget "https://rentry.org/sdmodels/pdf" -O "Stable Diffusion Models.pdf"
+wkhtmltopdf "https://wiki.installgentoo.com/wiki/Stable_Diffusion" "wiki.installgentoo.com Stable Diffusion Guide.pdf"
